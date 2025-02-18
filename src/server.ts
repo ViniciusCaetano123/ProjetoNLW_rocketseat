b5b19be3dbce65f0897ccc0ceb4249faf6ceccff
Vinicius Figueira Caetano <vinicaetano.f@gmail.com>
2025-02-17 22:15:35 -0300

    adicionando biome

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,38 @@
-import { fastify } from 'fastify'
-import { fastifyCors} from '@fastify/cors'
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
 import {
-    validatorCompiler,
-    serializerCompiler,
-    ZodTypeProvider
-} from 'fastify-type-provider-zod'
-import {z} from 'zod'
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+import { env } from "./env";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(fastifyCors,{
-    origin:true
-})
-app.post('/subscriptions',{
-    schema:{
-        body:z.object({
-            name:z.string(),
-            email:z.string().email()
-        })
-    }
-},(request,reply)=>{
-    const {name,email} = request.body
-    
-    return reply.status(201).send({
-        name,email
-    })
-})
-app.listen({port:3333}).then(()=>{
-    console.log('HTTP server running!')
-})
+app.register(fastifyCors, {
+	origin: true,
+});
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "NWL Connect",
+			version: "0.0.1",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, {
+	routePrefix: "/docs",
+});
+app.register(subscribeToEventRoute);
+
+app.listen({ port: env.PORT }).then(() => {
+	console.log("HTTP server running!");
+});
